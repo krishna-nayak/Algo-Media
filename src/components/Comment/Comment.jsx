418cea78feb3dafba4c80dc7a60a150904d7c99c
Krishna <nayak100krishna@gmail.com>
2023-05-28 14:44:49 +0530

    comment system base created UI
    just a simple comment base ui commpleted.

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,42 @@
+import { Box, Divider, IconButton, Input, Stack, ToggleButton, ToggleButtonGroup, Typography } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import React, { useState } from "react";
+
+const Comment = () => {
+  const [alignment, setAlignment] = useState("top");
+
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+  return (
+    <Box style={{ background: "#fff", border: "1px solid #bbb", borderRadius: "10px", padding: "20px" }}>
+      {/* TODO: TYPOGRAPHY */}
+      <Stack direction={"row"} justifyContent={"space-between"} marginY={"1rem"}>
+        <Typography variant="h5" component={"h5"} fontWeight="bold">
+          Comment
+        </Typography>
+        <ToggleButtonGroup size="small" color="primary" value={alignment} exclusive onChange={handleChange} aria-label="Platform">
+          <ToggleButton value="top">Top</ToggleButton>
+          <ToggleButton value="newest">Newest</ToggleButton>
+        </ToggleButtonGroup>
+      </Stack>
+      <Stack component="form" direction={"row"} gap={3} marginY={"1rem"} alignItems={"center"} justifyContent={"center"} sx={{ width: "100%" }} autoComplete="off">
+        {/* TODO: ICON user */}
+        <Box sx={{ display: "inline" }}>
+          <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" width={"40"} alt="profile-img" />
+        </Box>
+        <Input id="outlined-basic" name="comment" placeholder="Add a comment..." label="" variant="outlined" style={{ width: "80%" }} autoComplete="off" />
+        <IconButton aria-label="send" color="primary" size="large">
+          <SendIcon fontSize="inherit" />
+        </IconButton>
+      </Stack>
+      <Divider />
+
+      <div>Comment Sequence</div>
+
+      <div>Show more</div>
+    </Box>
+  );
+};
+
+export default Comment;
--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -8,7 +8,7 @@ import { Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 import { dateFormat } from "../utils/constants";
 import Loader from "./Loader";
-
+import Comment from "./Comment/Comment";
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
@@ -16,13 +16,9 @@ const VideoDetail = () => {
   const { id } = useParams();
 
   useEffect(() => {
-    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
-      setVideoDetail(data.items[0])
-    );
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) => setVideoDetail(data.items[0]));
 
-    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
-      (data) => setVideos(data.items)
-    );
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then((data) => setVideos(data.items));
   }, [id]);
 
   if (!videoDetail?.snippet) return <Loader />;
@@ -37,31 +33,17 @@ const VideoDetail = () => {
       {console.log(videoDetail)}
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
-          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
-            <ReactPlayer
-              url={`https://www.youtube.com/watch?v=${id}`}
-              className="react-player"
-              controls
-            />
+          {/* , position: "sticky", top: "86px" */}
+          <Box sx={{ width: "100%" }}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
             </Typography>
-            <Stack
-              direction="row"
-              justifyContent="space-between"
-              sx={{ color: "#fff" }}
-              py={1}
-              px={2}
-            >
+            <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2}>
               <Link to={`/channel/${channelId}`}>
-                <Typography
-                  variant={{ sm: "subtitle1", md: "h6" }}
-                  color="#fff"
-                >
+                <Typography variant={{ sm: "subtitle1", md: "h6" }} color="#fff">
                   {channelTitle}
-                  <CheckCircleIcon
-                    sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
-                  />
+                  <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
                 </Typography>
               </Link>
               <Stack direction="row" gap="20px" alignItems="center">
@@ -76,14 +58,10 @@ const VideoDetail = () => {
                 </Typography>
               </Stack>
             </Stack>
+            <Comment />
           </Box>
         </Box>
-        <Box
-          px={2}
-          py={{ md: 1, xs: 5 }}
-          justifyContent="center"
-          alignItems="center"
-        >
+        <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center">
           <Videos videos={videos} direction="column" />
         </Box>
       </Stack>
