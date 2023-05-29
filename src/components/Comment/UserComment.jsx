8921bc313f7c51c6d1f79f47510c172024a1c835
Krishna <nayak100krishna@gmail.com>
2023-05-29 07:15:36 +0530

    reply Btn functionality added

--- src/components/Comment/UserComment.jsx
+++ src/components/Comment/UserComment.jsx
@@ -1,6 +1,7 @@
 import { Box, Button, Divider, Stack, Typography } from "@mui/material";
 import React from "react";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import CommentInput from "./CommentInput";
 
 // This function is used to display the comment of the each user
 const UserComment = (props) => {
@@ -8,14 +9,16 @@ const UserComment = (props) => {
     message = "What's your favorite song by The Chainsmokers?",
     name = "keishna",
     profilePic = "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg",
-    reply = [{ name: "keishna", message: "I love the song Closer" }],
+    reply = [{ name: "krishna", message: "I love the song Closer" }],
   } = props;
+
+  const [showReplyInput, setShowReplyInput] = React.useState(false);
 
   return (
     <Box>
       <Stack direction={"row"} gap={2} sx={{ paddingY: "20px" }}>
         <img src={profilePic} width="50" height={"50"} alt="profile-pic" style={{ borderRadius: "100%", objectFit: "cover" }} />
-        <Box>
+        <Box sx={{ width: "100%" }}>
           <Typography variant="h6" component="h6" fontWeight="bold">
             {name}
           </Typography>
@@ -23,6 +26,12 @@ const UserComment = (props) => {
           <Typography variant="body1" component="p">
             {message}
           </Typography>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Button variant="text" color="secondary" onClick={() => setShowReplyInput((prev) => !prev)}>
+              {showReplyInput ? "Cancel" : "Reply"}
+            </Button>
+          </Box>
+          {showReplyInput && <CommentInput />}
           {reply.length > 0 && <Button startIcon={<ArrowDropDownIcon />}>{reply.length} Reply</Button>}
         </Box>
       </Stack>
